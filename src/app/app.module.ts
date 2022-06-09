5fc258c8a737739496376e4326e1ac2a0d0ef2c7
mauea22 <mauea22@hotmail.com>
2022-06-09 12:19:25 -0300

    RouterModule Rutas de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisModule,
     SharedModule
   ],
