18b690ded95ddafafb05fa202b0f23c0cb1d42d9
mauea22 <mauea22@hotmail.com>
2022-06-09 14:38:51 -0300

    sidebar.component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
   ],
   exports:[
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class SharedModule { }
