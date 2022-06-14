888f027e6e0a6a40c230e5848c428dda55b0bdd4
mauea22 <mauea22@hotmail.com>
2022-06-14 14:23:58 -0300

    optimizar las peticiones http

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 
@@ -10,30 +10,31 @@ export class PaisService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
+  //getter para optimizar la petición a la API
+  get httpParam(){
+    return new HttpParams().set('fields','cca2,name,population,capital,flags')
+  }
+
   constructor( private http: HttpClient) { }
 
   buscarPais( termino:string): Observable<Country[]>{
     //armo la url con apiUrl + /name / el pais que el usuario ingresa (termino) y la guardo en la variable url
     const url =`${this.apiUrl}/name/${termino}`;
-
-    //retorna el observable
-    return this.http.get<Country[]>(url);
+    //retorna el observable de la url optimizada en el get httpParam
+    return this.http.get<Country[]>(url,{params: this.httpParam});
   };
 
   buscarCapital( termino:string): Observable<Country[]>{
-
     const url =`${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params: this.httpParam});
   };
 
   buscarRegion( region:string): Observable<Country[]>{
-
     const url =`${this.apiUrl}/region/${region}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params: this.httpParam});
   };
 
   getPaisPorAlpha( id:string): Observable<any>{
-
     const url =`${this.apiUrl}/alpha/${id}`;
     return this.http.get<any>(url);
   };
